7c7334f0f30ac79b45519bfd3aef630a928d6d95
vishalrindhe <rindhevishal32@gmail.com>
2020-07-26 00:56:34 +0530

    create file,folders n connect local db taskmanager

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require('./database/mongoose');
+
 app.listen (3000, () => console.log("Server is connected on port: 3000") );
+ 
---
+++ backend/database/mongoose.js
@@ -0,0 +1,13 @@
+ const mongoose = require ('mongoose');
+
+ mongoose.Promise = global.Promise;
+
+//27017 is default port by mongodb
+//127.0.0.1 is custom protol by mongodb
+//taskmanager is db name
+
+ mongoose.connect('mongodb://127.0.0.1:27017/taskmanager' , { useNewUrlParser: true , useUnifiedTopology: true})
+    .then(() => console.log("Database is connected"))
+    .catch((error) => console.log("error"));
+
+module.exports = mongoose;
