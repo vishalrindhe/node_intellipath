d58eb075a775863c81b555120c2e65c9a29d3e79
vishalrindhe <rindhevishal32@gmail.com>
2020-07-30 02:01:14 +0530

    enable CORS n allow to use json  data

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,23 @@ const app = express();
 
 const mongoose = require('./database/mongoose');
 
-app.listen (3000, () => console.log("Server is connected on port: 3000") );
+/*
+CORS -CROSS ORIGIN REQUEST SECURITY
+localhost:3000 - backend api
+localhost:4200 - feontend 
+//copy the CORS code as it is
+it will allow only port 3000 and 4200
+*/
+
+app.use((req , res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE,");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+ 
+//allow app to use json data
+app.use(express.json());
+
+app.listen (3000, () => console.log("Server connected on port: 3000") );
  
--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -4,7 +4,7 @@
 
 //27017 is default port by mongodb
 //127.0.0.1 is custom protol by mongodb
-//taskmanager is db name
+//taskmanager is db name 
 
  mongoose.connect('mongodb://127.0.0.1:27017/taskmanager' , { useNewUrlParser: true , useUnifiedTopology: true})
     .then(() => console.log("Database is connected"))
