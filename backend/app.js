485d28e009e58de2b93b84ea4605abe35e15f9f6
vishalrindhe <rindhevishal32@gmail.com>
2020-08-03 02:23:40 +0530

    Crud Operations

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 const mongoose = require('./database/mongoose');
 
+const List = require('./database/models/list');
+const Task = require('./database/models/task');
+
+//allow app to use json data
+app.use(express.json());
 /*
 CORS -CROSS ORIGIN REQUEST SECURITY
 localhost:3000 - backend api
@@ -17,9 +22,53 @@ app.use((req , res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
- 
-//allow app to use json data
-app.use(express.json());
+/* Crud Operations add, update , Delete */
+/*
+Task need to pertform
+List: Create, update, ReadOne, ReadAll, Delete
+Task: Create, update, ReadOne, ReadAll, Delete
+*/
+/**
+ * GET -> GET data
+ * POST -> Save
+ * PUT & PATCH -> Update
+ * DELETE -> Delete
+ */
 
-app.listen (3000, () => console.log("Server connected on port: 3000") );
- 
+ /* Get All Lists */
+app.get('/lists', (req, res) => {
+    List.find({})
+    .then(lists => res.send(lists))
+    .catch((error) => console.log(error));
+});
+
+/* Add a List */
+app.post('/lists',(req, res) => {
+    (new List({ 'title': req.body.title }))
+        .save()
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+});
+
+/* Get Only one particular list */
+app.get('/lists/:listId', (req ,res) => {
+    List.find( { _id: req.params.listId })
+    .then((list) => res.send(list))
+    .catch((error) => console.log(error));
+});
+
+/* Upadte list */
+app.patch('/lists/:listId', (req ,res) => {
+    List.findOneAndUpdate({ '_id': req.params.listId }, { $set: req.body})
+    .then((list) => res.send(list))
+    .catch((error) => console.log(error));
+});
+
+/* Delete List */
+app.delete('/lists/:listId' , (req, res) => {
+    List.findByIdAndDelete(req.params.listId)
+    .then((list) => res.send(list))
+    .catch((error) => console.log(error));
+});
+
+app.listen (3000, () => console.log("Server connected on port: 3000"));
--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -6,7 +6,7 @@
 //127.0.0.1 is custom protol by mongodb
 //taskmanager is db name 
 
- mongoose.connect('mongodb://127.0.0.1:27017/taskmanager' , { useNewUrlParser: true , useUnifiedTopology: true})
+ mongoose.connect('mongodb://127.0.0.1:27017/taskmanager' , { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database is connected"))
     .catch((error) => console.log("error"));
 
