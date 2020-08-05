316fb943d6f6346bdebc7da1b3aa48bc08008ba0
vishalrindhe <rindhevishal32@gmail.com>
2020-08-05 17:10:21 +0530

    Crud operation for Tasks

--- backend/app.js
+++ backend/app.js
@@ -65,10 +65,54 @@ app.patch('/lists/:listId', (req ,res) => {
 });
 
 /* Delete List */
+/* this will delete List along with its tasks */
 app.delete('/lists/:listId' , (req, res) => {
-    List.findByIdAndDelete(req.params.listId)
-    .then((list) => res.send(list))
+    const deleteTasks = (list) => {
+        Task.deleteMany({ _listId: list._id})
+            .then(() => list)
+            .catch((error) => console.log(error));
+    };
+    const list = List.findByIdAndDelete(req.params.listId)
+            .then((list) => res.send(deleteTasks(list)))
+            .catch((error) => console.log(error));
+});
+
+/* URL should look like this :
+ http://localhost:3000/lists/:listId/tasks/:taskId */
+
+ /*Crud for Tasks */
+/*Get All Tasks */
+ app.get('/lists/:listId/tasks', (req, res) => {
+     Task.find({_listId: req.params.listId })
+     .then((tasks) => res.send(tasks))
+     .catch((error) => console.log(error));
+ });
+/* Create new Task */
+ app.post('/lists/:listId/tasks', (req, res) => {
+     (new Task({ '_listId': req.params.listId , 'title': req.body.title}))
+     .save()
+     .then((task) => res.send(task))
+     .catch((error) => console.log(error));
+ });
+/*Get One particular Task */
+ app.get('/lists/:listId/tasks/:taskId', (req , res) => {
+    Task.findOne({ '_listId': req.params.listId, '_id': req.params.taskId })
+    .then((task) => res.send(task))
     .catch((error) => console.log(error));
 });
+/* Update Task */
+app.patch('/lists/:listId/tasks/:taskId', (req , res) => {
+    Task.findOneAndUpdate({ '_listId': req.params.listId, '_id': req.params.taskId }, { $set: req.body })
+    .then((task) => res.send(task))
+    .catch((error) => console.log(error));
+});
+
+/* Delete Task */
+app.delete('/lists/:listId/tasks/:taskId', (req , res) => {
+    Task.findOneAndDelete({ '_listId': req.params.listId, '_id': req.params.taskId })
+    .then((task) => res.send(task))
+    .catch((error) => console.log(error));
+});
+
 
 app.listen (3000, () => console.log("Server connected on port: 3000"));
